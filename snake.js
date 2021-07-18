2faecec7188f43cd5e27bcf94720251eb497b1a6
HassanGG <hassan.g.ghnewa@gmail.com>
2021-07-18 15:16:19 +0100

    added some styling and added ids to grid

---
+++ snake.js
@@ -0,0 +1,10 @@
+let snake = [{x: 0, y: 0}];
+
+function clearScreen() {
+    let box = document.getElementsByClassName('box');
+    box.backgroundColor = "black"; 
+}
+
+console.log("yo");
+
+clearScreen();
