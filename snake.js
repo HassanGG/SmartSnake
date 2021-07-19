9781ab850d6af05e341d64915effe20014db7867
HassanGG <hassan.g.ghnewa@gmail.com>
2021-07-19 14:59:55 +0100

    added javascript to set pixels and draw snake

--- snake.js
+++ snake.js
@@ -1,13 +1,48 @@
-function clearScreen() {
-    for(let i = 0; i < dimension * dimension; i++) {
-        let box = document.getElementsByClassName('box')[i];
-        box.style.backgroundColor = "white";
-    }
-}
-
+/* global variables:
+dimension
+x
+y
+*/
 // makes sure the page is loaded before running the javascript
 window.onload = () => {
-    clearScreen();
-    console.log("yo");
+    // dimension should not be changed
+    const dimension = 20;
+    const frameInterval = 100;
+
+    let snake = [{x: 0, y: 0}];
+    // direction can be left, right, up or down
+    let direction = "";
+
+    function clearScreen() {
+        for(let i = 0; i < dimension * dimension; i++) {
+            let box = document.getElementsByClassName('box')[i];
+            box.style.backgroundColor = "white";
+        }
+    }
+
+    // this function takes in x, y coordinate and returns 
+    // string of div.
+    function getDiv(x, y) {
+        return "div" + '-' + y + '-' + x;
+    }
+
+    // this function changes a div's colour based on location.
+    function setPixel(x, y, colour) {
+        let div = document.getElementById(getDiv(x, y));
+        div.style.backgroundColor = colour;
+    }
+
+    function drawSnake() {
+        for(let i = 0; i < snake.length; i++) {
+            setPixel(snake[i].x, snake[i].y, "green");
+        }
+    }
+
+    function main() {
+        clearScreen();
+        drawSnake();
+        setInterval(main, frameInterval);
+    }
+
 }
 
